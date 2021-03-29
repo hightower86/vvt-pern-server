07e4a8a9ae7a1538ce1efdce7eeb789427909ec8
Adel <htwr86@gmail.com>
2021-03-29 20:07:57 +0300

    all works well

--- models/models.js
+++ models/models.js
@@ -3,10 +3,10 @@ const { DataTypes } = require('sequelize');
 
 const Blocks = sequelize.define('blocks', {
   id: { type: DataTypes.INTEGER, primaryKey: true, autoIncrement: true },
-  text: { type: DataTypes.STRING },
-  fontSize: { type: DataTypes.INTEGER },
-  color: { type: DataTypes.STRING },
-  bgColor: { type: DataTypes.STRING },
+  text: { type: DataTypes.STRING, defaultValue: '' },
+  fontSize: { type: DataTypes.INTEGER, defaultValue: 16 },
+  color: { type: DataTypes.STRING, defaultValue: '' },
+  bgColor: { type: DataTypes.STRING, defaultValue: '' },
 });
 
 module.exports = { Blocks };
